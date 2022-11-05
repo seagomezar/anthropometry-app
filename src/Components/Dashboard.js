592650a899643969630f61fcb7f7c5d39adf172d
Sebastian Gomez <seagomezar@gmail.com>
2022-11-04 22:04:39 -0500

    Integrating D3js

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,12 +1,44 @@
 // in src/Dashboard.js
 import * as React from 'react';
 import { Card, CardContent, CardHeader } from '@mui/material';
+import { useDataProvider, useGetList, useGetMany } from 'react-admin';
+import * as d3 from 'd3';
 
-const Dashboard = () => (
-  <Card>
-    <CardHeader title="Welcome to the administration" />
-    <CardContent>Lorem ipsum sic dolor amet...</CardContent>
-  </Card>
-);
+//https://medium.com/@imranfarooq0306/data-visualization-in-react-with-d3-js-d733cae0dca5
+const Circle = ({ number }) => {
+  const ref = React.useRef();
+
+  React.useEffect(() => {
+    const svgElement = d3.select(ref.current);
+    svgElement
+      .append('circle')
+      .attr('cx', 150)
+      .attr('cy', 70)
+      .attr('r', 50)
+      .style('fill', 'darkOrange');
+  }, []);
+
+  return (
+    <div>
+      {number}
+      <svg ref={ref} />
+      Users
+    </div>
+  );
+};
+
+const Dashboard = () => {
+  const dataProvider = useDataProvider();
+  const { data, isLoading, error } = useGetList('user');
+  return (
+    <Card>
+      <CardHeader title="Welcome to the administration" />
+      <CardContent>
+        <Circle number={data?.length} />
+        Lorem ipsum sic dolor amet...
+      </CardContent>
+    </Card>
+  );
+};
 
 export default Dashboard;
