3be5e96feee25f280a81209840590142b9106fb7
sergio perez <andresperezguerra5@gmail.com>
2022-11-22 16:38:40 -0500

    medicion edit

--- src/Components/Measurements.js
+++ src/Components/Measurements.js
@@ -64,15 +64,17 @@ export const MeasurementList = () => {
 
 export const MeasurementEdit = () => (
   <Edit>
-    <SimpleForm>
+    <SimpleForm sx={{ maxWidth: 500 }}>
       <NumberInput source="control" /> 
-      <DateInput source="creatinine" />
-      <NumberInput source="dm_elbow" />
-      <NumberInput source="dm_knee" />
-      <NumberInput source="dm_wrist" />
-      <NumberInput source="fitness_level" />
+      <Typography variant="h6" gutterBottom>
+        Mediciones
+      </Typography>
+      <NumberInput source="weight" />
       <NumberInput source="height" />
-      <TextInput source="id" />
+
+      <Typography variant="h6" gutterBottom>
+        Pliegues (m.m)
+      </Typography>
       <NumberInput source="plg_abdominal" />
       <NumberInput source="plg_armpit" />
       <NumberInput source="plg_calf" />
@@ -82,19 +84,37 @@ export const MeasurementEdit = () => (
       <NumberInput source="plg_supraspinal" />
       <NumberInput source="plg_thigh" />
       <NumberInput source="plg_triceps" />
+
+      <Typography variant="h6" gutterBottom>
+        Perímetros (cm)
+      </Typography>
       <NumberInput source="prm_arm" />
       <NumberInput source="prm_calf" />
       <NumberInput source="prm_chest" />
       <NumberInput source="prm_hip" />
       <NumberInput source="prm_thigh" />
       <NumberInput source="prm_waist" />
+
+      <Typography variant="h6" gutterBottom>
+      Diametro (cm)
+      </Typography>
+      <NumberInput source="dm_knee" />
+      <NumberInput source="dm_wrist" />
+      <NumberInput source="dm_elbow" />
+
+      <Typography variant="h6" gutterBottom>
+      Somatotipo de referencia
+      </Typography>
+      <NumberInput source="x" />
+      <NumberInput source="y" />
+
+      <DateInput source="creatinine" />
+      <NumberInput source="fitness_level" />
+      <TextInput source="id" />
       <TextInput source="t3_t4" />
       <TextInput source="triglycerides" />
       <TextInput source="uric_acid" />
       <ReferenceInput source="user_id" reference="user" />
-      <NumberInput source="weight" />
-      <NumberInput source="x" />
-      <NumberInput source="y" />
       <DateInput source="created_at" />
       <DateInput source="updated_at" />
     </SimpleForm>
