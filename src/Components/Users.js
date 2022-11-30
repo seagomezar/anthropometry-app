589a7dbdd06232a98937c1d3559c7aa35d20d9c6
sergio perez <andresperezguerra5@gmail.com>
2022-11-30 16:34:55 -0500

    traduccion

--- src/Components/Users.js
+++ src/Components/Users.js
@@ -100,7 +100,7 @@ export const UserEdit = () => {
         <Separator />
 
         <Typography variant="h6" gutterBottom>
-          Address & Phone
+        {translate('myroot.addressAndPhone')}
         </Typography>
         <TextInput
           source="address"
@@ -112,7 +112,7 @@ export const UserEdit = () => {
         <Separator />
 
         <Typography variant="h6" gutterBottom>
-          Password
+        {translate('myroot.password')}
         </Typography>
         <Box display={{ xs: 'block', sm: 'flex' }}>
           <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
