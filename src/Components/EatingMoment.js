5c8afb15c25d8f436095e9a6199b575cb61da9f2
sergio perez <andresperezguerra5@gmail.com>
2022-12-01 15:21:23 -0500

    eating moment traduccion show

--- src/Components/EatingMoment.js
+++ src/Components/EatingMoment.js
@@ -8,6 +8,7 @@ import {
   Edit,
   SimpleForm,
   TextInput,
+  useTranslate,
   Create,
   Show,
   SimpleShowLayout,
@@ -62,17 +63,26 @@ export const EatingMomentCreate = () => (
   </Create>
 );
 
-export const EatingMomentShow = () => (
+export const EatingMomentShow = () => {
+  const translate = useTranslate();
+  return (
  <Show>
    <SimpleShowLayout sx={{ maxWidth: 500 }}>
    <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
       <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
-      <TextInput source="name"fullWidth />
+      <Typography variant="h6" gutterBottom>
+              {translate('resources.eating_moment.fields.name')}
+            </Typography>
+      <TextField source="name"fullWidth />
       </Box>
       <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
-      <TextInput source="time"fullWidth />
+      <Typography variant="h6" gutterBottom>
+              {translate('resources.eating_moment.fields.time')}
+            </Typography>
+      <TextField source="time"fullWidth />
       </Box>
     </Box>
    </SimpleShowLayout>
  </Show>
-)
+  );
+};
