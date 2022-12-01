cc7225df45fb37acd7e3dec1c4ee0228f879743e
sergio perez <andresperezguerra5@gmail.com>
2022-12-01 15:26:21 -0500

    nutritionist traduccion show

--- src/Components/Nutritionist.js
+++ src/Components/Nutritionist.js
@@ -9,6 +9,7 @@ import {
   Edit,
   SimpleForm,
   TextInput,
+  useTranslate,
   Create,
   Show,
   SimpleShowLayout,
@@ -88,30 +89,48 @@ export const NutritionistCreate = () => (
   </Create>
 );
 
-export const NutritionistShow = () => (
+export const NutritionistShow = () => {
+  const translate = useTranslate();
+  return (
   <Show>
      <SimpleShowLayout sx={{ maxWidth: 500 }}>
      <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
      <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+     <Typography variant="h6" gutterBottom>
+              {translate('resources.nutritionist.fields.firstname')}
+            </Typography>
      <TextField source="firstname"fullWidth />
     </Box>
     <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+    <Typography variant="h6" gutterBottom>
+              {translate('resources.nutritionist.fields.lastname')}
+            </Typography>
      <TextField source="lastname"fullWidth />
       </Box>
     </Box>
     <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
      <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+     <Typography variant="h6" gutterBottom>
+              {translate('resources.nutritionist.fields.address')}
+            </Typography>
      <TextField source="address"fullWidth />
       </Box>
       <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+      <Typography variant="h6" gutterBottom>
+              {translate('resources.nutritionist.fields.email')}
+            </Typography>
       <TextField source="email"fullWidth />
       </Box>
     </Box>
     <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
-     <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}> 
+     <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+     <Typography variant="h6" gutterBottom>
+              {translate('resources.nutritionist.fields.phone')}
+            </Typography> 
      <TextField source="phone"fullWidth />
      </Box>
     </Box>
     </SimpleShowLayout>
   </Show>
-)
+  );
+};
