16acfd71c5f14e0c61b17ae9770ed84e92b7a988
sergio perez <andresperezguerra5@gmail.com>
2022-12-01 14:45:37 -0500

    food traduccion

--- src/Components/Food.js
+++ src/Components/Food.js
@@ -10,6 +10,7 @@ import {
   NumberInput,
   SimpleForm,
   TextInput,
+  useTranslate,
   Create,
   Show,
   SimpleShowLayout,
@@ -109,38 +110,61 @@ export const FoodCreate = () => (
   </Create>
 );
 
-export const FoodShow = () => (
+export const FoodShow = () => {
+  const translate = useTranslate();
+  return (
  <Show>
   <SimpleShowLayout sx={{ maxWidth: 500 }}>
   <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
       <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
-      <NumberInput source="calories"fullWidth />
+      <Typography variant="h6" gutterBottom>
+              {translate('resources.food.fields.calories')}
+            </Typography>
+      <NumberField source="calories"fullWidth />
       </Box>
       <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
-      <NumberInput source="chos"fullWidth />
+      <Typography variant="h6" gutterBottom>
+              {translate('resources.food.fields.chos')}
+            </Typography>
+      <NumberField source="chos"fullWidth />
       </Box>
      </Box>
      <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
       <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
-      <TextInput source="description"fullWidth />
+      <Typography variant="h6" gutterBottom>
+              {translate('resources.food.fields.description')}
+            </Typography>
+      <TextField source="description"fullWidth />
       </Box>
       <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
-      <NumberInput source="fat"fullWidth />
+      <Typography variant="h6" gutterBottom>
+              {translate('resources.food.fields.fat')}
+            </Typography>
+      <NumberField source="fat"fullWidth />
       </Box>
      </Box>
      <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
       <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
-      <TextInput source="measure_unit"fullWidth />
+      <Typography variant="h6" gutterBottom>
+              {translate('resources.food.fields.measure_unit')}
+            </Typography>
+      <TextField source="measure_unit"fullWidth />
       </Box>
       <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
-      <NumberInput source="protein"fullWidth />
+      <Typography variant="h6" gutterBottom>
+              {translate('resources.food.fields.protein')}
+            </Typography>
+      <NumberField source="protein"fullWidth />
       </Box>
      </Box>
      <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
-     <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
-     <NumberInput source="quantity"fullWidth />
+     <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}><Typography variant="h6" gutterBottom>
+              {translate('resources.food.fields.quantity')}
+            </Typography>
+     <NumberField source="quantity"fullWidth />
      </Box>
      </Box>
   </SimpleShowLayout>
  </Show>
-)
+ );
+};
