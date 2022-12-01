f0d9c05a521c29c48f5c1eb511e24bbae2e935f7
sergio perez <andresperezguerra5@gmail.com>
2022-12-01 15:17:08 -0500

    food has food category traduccion show

--- src/Components/FoodHasFoodCategory.js
+++ src/Components/FoodHasFoodCategory.js
@@ -10,6 +10,7 @@ import {
   ReferenceInput,
   SimpleForm,
   TextInput,
+  useTranslate,
   Create,
   Show,
   SimpleShowLayout,
@@ -64,17 +65,26 @@ export const FoodHasFoodCategoryCreate = () => (
   </Create>
 );
 
-export const FoodHasFoodCategoryShow = () => (
+export const FoodHasFoodCategoryShow = () => {
+  const translate = useTranslate();
+  return (
  <Show>
    <SimpleShowLayout sx={{ maxWidth: 500 }}>
    <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
      <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+     <Typography variant="h6" gutterBottom>
+              {translate('resources.food_has_food_category.fields.food_category_id')}
+            </Typography>
      <ReferenceField source="food_category_id"reference="food_category"fullWidth/>
       </Box>
       <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+      <Typography variant="h6" gutterBottom>
+              {translate('resources.food_has_food_category.fields.food_id')}
+            </Typography>
       <ReferenceField source="food_id" reference="food"fullWidth />
       </Box>
       </Box>
    </SimpleShowLayout>
  </Show>
-)
+  );
+};
