ca14ffd45cc46015b754660bba1994fe680d5ca9
sergio perez <andresperezguerra5@gmail.com>
2022-12-01 14:57:53 -0500

    goal traduccion de show

--- src/Components/Goal.js
+++ src/Components/Goal.js
@@ -8,6 +8,7 @@ import {
   Edit,
   SimpleForm,
   TextInput,
+  useTranslate,
   Create,
   Show,
   SimpleShowLayout,
@@ -52,14 +53,20 @@ export const GoalCreate = () => (
   </Create>
 );
 
-export const GoalShow = () => (
-<Show>
-  <SimpleShowLayout sx={{ maxWidth: 500 }}>
+export const GoalShow = () => {
+  const translate = useTranslate();
+  return (
+  <Show>
+     <SimpleShowLayout sx={{ maxWidth: 500 }}>
     <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
      <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+     <Typography variant="h6" gutterBottom>
+              {translate('resources.goal.fields.name')}
+            </Typography>
      <TextField source="name"fullWidth />
       </Box>
       </Box>
-  </SimpleShowLayout>
-</Show>
-);
+     </SimpleShowLayout>
+  </Show>
+  );
+};
