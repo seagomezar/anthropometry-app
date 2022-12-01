deeb75b4e158c1d0164c36fa7c7269b85a8cd2fb
sergio perez <andresperezguerra5@gmail.com>
2022-12-01 15:12:56 -0500

    food eating moment traduccion show

--- src/Components/FoodHasEatingMomment.js
+++ src/Components/FoodHasEatingMomment.js
@@ -10,6 +10,7 @@ import {
   ReferenceInput,
   SimpleForm,
   TextInput,
+  useTranslate,
   Create,
   Show,
   SimpleShowLayout,
@@ -70,20 +71,29 @@ export const FoodHasEatingMomentCreate = () => (
   </Create>
 );
 
-export const FoodHasEatingMomentShow = () => (
+export const FoodHasEatingMomentShow = () => {
+  const translate = useTranslate();
+  return (
  <Show>
   <SimpleShowLayout sx={{ maxWidth: 500 }}>
   <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
      <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+     <Typography variant="h6" gutterBottom>
+              {translate('resources.food_has_eating_moment.fields.eating_moment_id')}
+            </Typography>
      <ReferenceField
         source="eating_moment_id"
         reference="eating_moment"
       />
      </Box>
      <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+     <Typography variant="h6" gutterBottom>
+              {translate('resources.food_has_eating_moment.fields.food_id')}
+            </Typography>
       <ReferenceField source="food_id" reference="food"fullWidth />
      </Box>
     </Box>
    </SimpleShowLayout>
  </Show>
-)
+ );
+};
