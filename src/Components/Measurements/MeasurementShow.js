7c131ae6ebf9dd75787302374b50973e27d4f596
Sergio Perez <andresperezguerra5@gmail.com>
2022-12-09 18:14:19 -0500

    measure

--- src/Components/Measurements/MeasurementShow.js
+++ src/Components/Measurements/MeasurementShow.js
@@ -72,47 +72,76 @@ export const MeasurementShow = () => {
               <NumberField source="height" fullWidth />
             </Box>
           </Box>
-  
           <Typography variant="h6" gutterBottom>
             {translate('myroot.pliegues')}
           </Typography>
           <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
             <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.plg_abdominal')}
+              </Typography>
               <NumberField source="plg_abdominal" fullWidth />
             </Box>
             <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.plg_armpit')}
+              </Typography>
               <NumberField source="plg_armpit" fullWidth />
             </Box>
           </Box>
           <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
             <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.plg_calf')}
+              </Typography>
               <NumberField source="plg_calf" fullWidth />
             </Box>
             <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.plg_chest')}
+              </Typography>
               <NumberField source="plg_chest" fullWidth />
             </Box>
           </Box>
           <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
             <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.plg_subscapular')}
+              </Typography>
               <NumberField source="plg_subscapular" fullWidth />
             </Box>
             <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.plg_suprailiac')}
+              </Typography>
               <NumberField source="plg_suprailiac" fullWidth />
             </Box>
           </Box>
           <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
             <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.plg_supraspinal')}
+              </Typography>
               <NumberField source="plg_supraspinal" fullWidth />
             </Box>
             <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.plg_thigh')}
+              </Typography>
               <NumberField source="plg_thigh" fullWidth />
             </Box>
           </Box>
           <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
             <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.plg_triceps')}
+              </Typography>
               <NumberField source="plg_triceps" fullWidth />
             </Box>
             <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.plg_bicep')}
+              </Typography>
             <NumberField source="plg_bicep" fullWidth />
             </Box>
           </Box>
@@ -121,30 +150,51 @@ export const MeasurementShow = () => {
           </Typography>
           <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
             <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.prm_arm')}
+              </Typography>
               <NumberField source="prm_arm" fullWidth />
             </Box>
             <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.prm_calf')}
+              </Typography>
               <NumberField source="prm_calf" fullWidth />
             </Box>
           </Box>
           <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
             <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.prm_chest')}
+              </Typography>
               <NumberField source="prm_chest" fullWidth />
             </Box>
             <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.prm_hip')}
+              </Typography>
               <NumberField source="prm_hip" fullWidth />
             </Box>
           </Box>
           <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
             <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.prm_thigh')}
+              </Typography>
               <NumberField source="prm_thigh" fullWidth />
             </Box>
             <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.prm_waist')}
+              </Typography>
               <NumberField source="prm_waist" fullWidth />
             </Box>
           </Box>
           <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
           <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+          <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.prm_wrist')}
+              </Typography>
               <NumberField source="prm_wrist" fullWidth />
             </Box>
           </Box>
@@ -153,14 +203,23 @@ export const MeasurementShow = () => {
           </Typography>
           <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
             <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.dm_elbow')}
+              </Typography>
               <NumberField source="dm_elbow" fullWidth />
             </Box>
             <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.dm_knee')}
+              </Typography>
               <NumberField source="dm_knee" fullWidth />
             </Box>
           </Box>
           <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
             <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.dm_wrist')}
+              </Typography>
               <NumberField source="dm_wrist" fullWidth />
             </Box>
           </Box>
@@ -169,30 +228,51 @@ export const MeasurementShow = () => {
           </Typography>
           <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
             <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.x')}
+              </Typography>
               <NumberField source="x" fullWidth />
             </Box>
             <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.y')}
+              </Typography>
               <NumberField source="y" fullWidth />
             </Box>
           </Box>
           <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
             <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.creatinine')}
+              </Typography>
               <DateField source="creatinine" fullWidth />
             </Box>
             <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.fitness_level')}
+              </Typography>
               <NumberField source="fitness_level" fullWidth />
             </Box>
           </Box>
           <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
             <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.t3_t4')}
+              </Typography>
               <TextField source="t3_t4" fullWidth />
             </Box>
             <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.triglycerides')}
+              </Typography>
               <TextField source="triglycerides" fullWidth />
             </Box>
           </Box>
           <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
             <Box flex={1} mr={{ xs: 0, sm: '0.5em' }}>
+            <Typography variant="h6" gutterBottom>
+                {translate('resources.measurement.fields.uric_acid')}
+              </Typography>
               <TextField source="uric_acid" fullWidth />
             </Box>
           </Box>
