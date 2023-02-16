d0cd7e16ebe1e94cecdd33eeaeae731040f048dc
Sebastian Gomez <seagomezar@gmail.com>
2023-02-16 17:31:22 -0500

    feat: Colocandole estilos al PDF

--- src/Components/Measurements/ExportablePDF.js
+++ src/Components/Measurements/ExportablePDF.js
@@ -7,6 +7,8 @@ import ReactPDF, {
     PDFViewer,
     PDFDownloadLink,
   } from "@react-pdf/renderer";
+
+
   const styles = StyleSheet.create({
     page: {
       flexDirection: "row",
@@ -16,25 +18,70 @@ import ReactPDF, {
       margin: 10,
       padding: 10,
       flexGrow: 1,
+      width: '50%'
     },
+    column:{ // Columna
+      fontSize: 12
+    },
+    row: { // Fila
+      flexDirection: 'row'
+    },
+    fondoAzul: {
+      backgroundColor: "rgb(97, 161, 235)"
+    },
+    fondVerde: {
+      backgroundColor: 'rgb(38, 100, 38)'
+    }
   });
-// Create Document Component
+
+
+  // Create Document Component
 export const ExportablePDF = ({ record, results, translate }) => {
     // TODO: Implement this propertly to display the data on PDF
     return (
       <Document>
         <Page size="A4" style={styles.page}>
           <View style={styles.section}>
-            <Text>
+
+            <View style={{...styles.row, ...styles.fondoAzul}}>
+              <Text style={styles.column}>
+                {record.nutritionist_id}
+              </Text>
+            </View>
+
+            <View style={styles.row}>
+              <Text style={styles.column}>
+                {translate("resources.measurement.fields.user_id")}
+              </Text>
+              <Text style={{...styles.column, ...styles.fondoAzul}}>
+                {record.user_id}
+              </Text>
+            </View>
+
+
+
+            <View style={styles.row}>
+
+            <Text style={styles.column}>
               {translate(
                 "resources.measurement.fields.desiredFat2MethodPercentage"
               )}
             </Text>
-            <Text>{results.desiredFat2MethodPercentage.toFixed(2)}</Text>
+
+            <Text style={styles.text}>{results.desiredFat2MethodPercentage.toFixed(2)}</Text>
+
           </View>
+            
+          </View>
+
+
+          {/* Este es el otro lado */}
           <View style={styles.section}>
-            <Text>{translate("resources.measurement.fields.endomorph")}</Text>
-            <Text>{results.endomorph}</Text>
+
+                <View style={{...styles.row, ...styles.fondVerde}}>
+                  <Text>{" "}</Text>
+                </View>
+            
           </View>
         </Page>
       </Document>
