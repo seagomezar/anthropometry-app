b4fa71a6072a2a5862b6636aa9ef11e681e569bc
Sergio Perez <andresperezguerra5@gmail.com>
2023-04-12 18:25:20 -0500

    feat(PhaysicalTest): mejorar la lista

--- src/Components/PhysicalTest/PhysicalTestList.js
+++ src/Components/PhysicalTest/PhysicalTestList.js
@@ -9,16 +9,27 @@ import {
   NumberField,
   TextInput,
   ReferenceField,
+  DateField
 } from "react-admin";
 import { useMediaQuery } from "@mui/material";
 
 const physicalTestFilters = [
   <TextInput  label="Search" source="position@_like" alwaysOn/>,
 ];
-
 export const PhysicalTestList = () => {
+  const isSmall = useMediaQuery((theme) => theme.breakpoints.down("sm"));
   return (
     <List filters={physicalTestFilters}>
+      {isSmall ? (
+        <SimpleList
+          primaryText={(record) => <ReferenceField record={record} source="athlete_id" reference="athlete" /> }
+          secondaryText={(record) => `${record.height} ${record.weight} ${record.position} `}
+          tertiaryText={(record) => (
+            <DateField record={record} source="created_at" />
+          )}
+          rowStyle={(record) => ({backgroundColor: "lightblue"})}
+        />
+      ) : (
         <Datagrid rowClick="show">
           <TextField source="id" />
           <TextField source="position" />
@@ -35,6 +46,8 @@ export const PhysicalTestList = () => {
           <EditButton />
           <DeleteButton />
         </Datagrid>
+         )}
     </List>
   );
 };
+
